849ec57e8a761c21ba936fb1a112c604669139f0
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-02-22 22:48:32 +1100

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes } from "react-router-dom"
+import "./App.css"
+import SearchAppBar from "./components/SearchAppBar"
+import HomePage from "./pages/HomePage"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { ThemeProvider, createTheme, CssBaseline } from "@mui/material"
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles"
+import { blue, deepOrange, grey, purple, red } from "@mui/material/colors"
+
+const theme = createTheme({
+	// palette: {
+	// 	mode: "dark",
+	// 	primary: deepOrange,
+	// 	divider: deepOrange[700],
+
+	// 	text: {
+	// 		primary: "#fff",
+	// 		secondary: grey[500],
+	// 	},
+	// },
+	palette: {
+		mode: "dark",
+		primary: {
+			main: "#3f51b5",
+		},
+		secondary: {
+			main: "#f50057",
+		},
+	},
+})
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+])
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<SearchAppBar />
+				<RouterProvider router={router} />
+			</ThemeProvider>
+		</div>
+	)
 }
 
-export default App;
+export default App
